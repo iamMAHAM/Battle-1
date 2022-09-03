cade7eb1bfef3ec4c3c1e3b73acfa443a6093089
iamMAHAM <abdulkabore@gmail.com>
2022-09-03 12:43:34 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
+const routes = require("./route")
 
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.set("view engine", "ejs")
 
-app.listen(3000, ()=>{console.log("server demmaré sur le port 8080")})
+app.use("/", routes)
+app.listen(3000, ()=>{console.log("server demmaré sur le port 3000")})
